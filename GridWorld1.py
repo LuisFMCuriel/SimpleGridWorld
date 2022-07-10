6faec7acdc826f67ddd4a9f0654256141809234e
Luis <felipemoralescuriel@gmail.com>
2022-07-10 08:45:28 -0500

    Update GridWorld1.py

--- GridWorld1.py
+++ GridWorld1.py
@@ -2,33 +2,35 @@
 import numpy as np
 
 BOARD_ROWS = 4
-BOARD_COLS = 4
+BOARD_COLS = 4	
 DETERMINISTIC = True
-WIN_STATE = (0, 3)
-LOSE_STATE = (1, 3)
-START = (2, 1)
+WIN_STATE = (3,3)
+LOSE_STATE = [(1,1), (1,3), (2,3), (3,0)]
+START = (0, 0)
 
 class Env:
 
-	def __init__(self, state):
+	def __init__(self, state, probs, DETERMINISTIC=False):
 		self.state = state
 		self.board = np.zeros([BOARD_ROWS, BOARD_COLS])
 		self.board[WIN_STATE] = 2
-		self.board[LOSE_STATE] = -1
+		self.probabilities = probs
+		for i in LOSE_STATE:
+			self.board[i] = -1
 		self.isEnd = False
 		self.actions = {0:"up", 1:"down", 2:"left", 3:"right"}
-		self.determine = DETERMINISTIC
+		self.deterministic = DETERMINISTIC
 
 	def giveReward(self, state, past_state):
 		if state == WIN_STATE and past_state != WIN_STATE:
 			return 1
-		elif state == LOSE_STATE:
+		elif state in LOSE_STATE:
 			return 0#-1
 		else:
 			return 0
 
 
-	def nxtPosition(self, action, state):
+	def nxtPosition(self, action, state, prob=1):
 		"""
 		action: up, down, left, right
 		-------------
@@ -37,24 +39,63 @@ class Env:
 		2 |
 		return next position
 		"""
-		if self.determine:
-			if action == "up":
-				nxtState = (state[0] - 1, state[1])
-			elif action == "down":
-				nxtState = (state[0] + 1, state[1])
-			elif action == "left":
-				nxtState = (state[0], state[1] - 1)
+		if self.deterministic:
+			if state == WIN_STATE or state in LOSE_STATE:
+				return state
 			else:
-				nxtState = (state[0], state[1] + 1)
-			# if next state legal
-			if (nxtState[0] >= 0) and (nxtState[0] <= BOARD_ROWS-1):
-				if (nxtState[1] >= 0) and (nxtState[1] <= BOARD_COLS-1):
-					#if nxtState != (1, 1):
-					return nxtState
-			return state
+				if action == "up":
+					nxtState = (state[0] - 1, state[1])
+				elif action == "down":
+					nxtState = (state[0] + 1, state[1])
+				elif action == "left":
+					nxtState = (state[0], state[1] - 1)
+				else:
+					nxtState = (state[0], state[1] + 1)
+				# if next state legal
+				if (nxtState[0] >= 0) and (nxtState[0] <= BOARD_ROWS-1):
+					if (nxtState[1] >= 0) and (nxtState[1] <= BOARD_COLS-1):
+						#if nxtState != (1, 1):
+						return nxtState
+				return state
+		else: #If the environment is not deterministic
+			if state == WIN_STATE or state in LOSE_STATE:
+				return state
+			else:
+				if prob == "GoOn":#Go to the intended state
+					if action == "up":
+						nxtState = (state[0] - 1, state[1])
+					elif action == "down":
+						nxtState = (state[0] + 1, state[1])
+					elif action == "left":
+						nxtState = (state[0], state[1] - 1)
+					else:
+						nxtState = (state[0], state[1] + 1)
+					# if next state legal
+					if (nxtState[0] >= 0) and (nxtState[0] <= BOARD_ROWS-1):
+						if (nxtState[1] >= 0) and (nxtState[1] <= BOARD_COLS-1):
+							#if nxtState != (1, 1):
+							return nxtState
+					return state
+				elif prob == "Reverse": #Go reverse of the intended state
+					if action == "down":
+						nxtState = (state[0] - 1, state[1])
+					elif action == "up":
+						nxtState = (state[0] + 1, state[1])
+					elif action == "right":
+						nxtState = (state[0], state[1] - 1)
+					else:
+						nxtState = (state[0], state[1] + 1)
+					# if next state legal
+					if (nxtState[0] >= 0) and (nxtState[0] <= BOARD_ROWS-1):
+						if (nxtState[1] >= 0) and (nxtState[1] <= BOARD_COLS-1):
+							#if nxtState != (1, 1):
+							return nxtState
+					return state
+				else: #Stay in the same place
+					return state
 
 	def isEndFunc(self, state, past_state):
-		if (state == WIN_STATE) or (state == LOSE_STATE) or (past_state == WIN_STATE) or (past_state == LOSE_STATE):
+		if (state == WIN_STATE) or (state in LOSE_STATE) or (past_state == WIN_STATE) or (past_state in LOSE_STATE):
 			return True
 		else:
 			return False
@@ -91,16 +132,29 @@ class Env:
 		MDP = {}
 		Dic_location = self.IdxtoLocation
 		for state in range(BOARD_ROWS*BOARD_COLS):
+			MDP[state] = {}
 			for action in range(len(self.actions)):
-				Nposition = self.nxtPosition(self.actions[action], Dic_location[state])
-				#print(self.actions[action], Dic_location[state], Nposition)
-				IdxNposition = list(Dic_location.keys())[list(Dic_location.values()).index(Nposition)]
+				MDP[state][action] = []
 
-				if DETERMINISTIC:
-					try:
-						MDP[state][action] = [(1.0, IdxNposition, self.giveReward(Nposition, Dic_location[state]), self.isEndFunc(Nposition, Dic_location[state]))]
-					except:
-						MDP[state] = {action : [(1.0, IdxNposition, self.giveReward(Nposition, Dic_location[state]), self.isEndFunc(Nposition, Dic_location[state]))]}
+				if self.deterministic:
+					Nposition = self.nxtPosition(self.actions[action], Dic_location[state])
+					#print(self.actions[action], Dic_location[state], Nposition)
+					IdxNposition = list(Dic_location.keys())[list(Dic_location.values()).index(Nposition)]
+					#try:
+					MDP[state][action] = [(1.0, IdxNposition, self.giveReward(Nposition, Dic_location[state]), self.isEndFunc(Nposition, Dic_location[state]))]
+					#except:
+					#	MDP[state] = {action : [(1.0, IdxNposition, self.giveReward(Nposition, Dic_location[state]), self.isEndFunc(Nposition, Dic_location[state]))]}
+				else:
+					for prob in self.probabilities.keys():
+						Nposition = self.nxtPosition(self.actions[action], Dic_location[state], self.probabilities[prob])
+						#print(self.actions[action], Dic_location[state], Nposition)
+						IdxNposition = list(Dic_location.keys())[list(Dic_location.values()).index(Nposition)]
+						#try:
+						MDP[state][action] += [(prob, IdxNposition, self.giveReward(Nposition, Dic_location[state]), self.isEndFunc(Nposition, Dic_location[state]))]
+						#except:
+						#	MDP[state] = {action : [(prob, IdxNposition, self.giveReward(Nposition, Dic_location[state]), self.isEndFunc(Nposition, Dic_location[state]))]}
+						
+
 		return MDP
 
 """class Agent:
@@ -126,20 +180,35 @@ def value_iteration(P, gamma=1.0, theta=1e-10):
 		for s in range(len(P)):
 			for a in range(len(P[s])):
 				for prob, next_state, reward, done in P[s][a]:
-					Q[s][a] += prob * (reward+gamma*V[next_state]*(not done))
+					Q[s][a] += prob * (reward + gamma * V[next_state] * (not done))
 		if np.max(np.abs(V - np.max(Q, axis=1))) < theta:
 			break
-
+		#print(Q)
+		#print(V)
 		V = np.max(Q, axis=1)
-	pi = lambda: {s:a for s, a in enumerate(np.argmax(Q, axis=1))}[s]
+	pi = lambda s: {s:a for s, a in enumerate(np.argmax(Q, axis=1))}[s]
 	return V, pi
 
 
+def print_policy(pi, P, action_symbols=('^', 'v', '<', '>'), n_cols=4, title='Policy:'):
+    print(title)
+    arrs = {k:v for k,v in enumerate(action_symbols)}
+    for s in range(len(P)):
+        a = pi(s)
+        print("| ", end="")
+        if np.all([done for action in P[s].values() for _, _, _, done in action]):
+            print("".rjust(9), end=" ")
+        else:
+            print(str(s).zfill(2), arrs[a].rjust(6), end=" ")
+        if (s + 1) % n_cols == 0: print("|")
 
-env = Env(START)
+
+env = Env(START, probs = {0.5:"GoOn", 0.33:"Stay", 0.166:"Reverse"}, DETERMINISTIC=False)
 env.showBoard()
 MDP = env.P()
-print(MDP)
+V, pi = value_iteration(P=MDP)
+print(V)
+print_policy(pi, MDP, n_cols=BOARD_COLS)
 #agent = Agent(env.actions, env.state)
-V, policy = value_iteration(P=MDP)
-print(policy)
+#print(policy)
+
